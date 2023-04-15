f98b17bcaa9b54d0002f29a31d0301a7e56c28cb
MotoShin <m8o8t2o8@gmail.com>
2023-04-15 02:29:23 +0000

    ✨ add retry process

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ from translater import Translater
 from tqdm import tqdm
 import sys
 import time
+import os
+import shutil
+
+PROCESSING_INDEX_FILE = 'result/processing_index.txt'
+PROCESSING_TRANSLATE_FILE = 'result/processing_translate.txt'
+RESULT_FILE = 'result/result.txt'
 
 def main():
     args = sys.argv
@@ -11,6 +17,13 @@ def main():
         target_pdf_file = "sample.pdf"
     else:
         target_pdf_file = args[1]
+
+    processing_index = 0
+    # 前回の実行で失敗している箇所を取得
+    if(os.path.isfile(PROCESSING_INDEX_FILE)):
+        f = open(PROCESSING_INDEX_FILE, 'r')
+        processing_index = int(f.read())
+        f.close()
 
     # 使用するクラスのインスタンス生成
     converter = Converter(target_pdf_file=target_pdf_file)
@@ -26,14 +39,29 @@ def main():
     sentencies = formatter.sentence_split(text_shaped)
 
     # chatGPTを使って和訳
-    result = ""
-    for sentence in tqdm(sentencies):
+    for i, sentence in enumerate(tqdm(sentencies)):
+        if i < processing_index:
+            continue
+
+        # 進捗をtxtファイルに上書き
+        with open(PROCESSING_INDEX_FILE, 'w') as file:
+            file.write(str(i))
+
         jp_sentence = translater.translate(sentence)
-        result += sentence + "\n" + jp_sentence + "\n\n"
+        result = sentence + "\n" + jp_sentence + "\n\n"
+
+        # 結果をtxtファイルに追記
+        with open(PROCESSING_TRANSLATE_FILE, 'a') as file:
+            file.write(result)
+
         time.sleep(25)
 
-    # 結果をtxtファイルに出力
-    with open('result/result.txt', 'w') as file:
-        file.write(result)
+    shutil.copyfile(PROCESSING_TRANSLATE_FILE, RESULT_FILE)
+
+    # 正常終了したらprocessing.txtを削除する
+    if(os.path.isfile(PROCESSING_INDEX_FILE)):
+        os.remove(PROCESSING_INDEX_FILE)
+    if(os.path.isfile(PROCESSING_TRANSLATE_FILE)):
+        os.remove(PROCESSING_TRANSLATE_FILE)
 
 main()
