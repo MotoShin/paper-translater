d53dc760a2a0b1867e98dfa77f008f281bba2415
MotoShin <m8o8t2o8@gmail.com>
2023-09-15 07:12:05 +0000

    日本語から英語を追加

--- translater.py
+++ translater.py
@@ -26,3 +26,22 @@ class Translater():
         )
 
         return response.choices[0]["message"]["content"].strip()
+
+@dataclass
+class Translater2():
+    def __post_init__(self):
+        self.token = os.environ["OPENAI_API_KEY"]
+
+    def translate(self, text: str) -> str:
+        return self._post("下記の文章を英文はそのままで日本語は英訳してください\n" + text)
+
+    @retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(5))
+    def _post(self, text: str) -> str:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": text},
+            ],
+        )
+
+        return response.choices[0]["message"]["content"].strip()
